e731b27107dc982bfecc38f6d36f653576491913
Weiss Luxford <weissluxford@gmail.com>
2022-06-25 17:09:27 +0500

    reverse_int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    function reverseInt(number) {
+        let a = number.toString().split("").reverse().join('')
+        a = (a[0] == 0) ? a.substring(1) : (a[a.length - 1] == "-") ? a.slice(0, a.length - 1) : a ;
+        return +a
+    }
+    return reverseInt(n)
 }
